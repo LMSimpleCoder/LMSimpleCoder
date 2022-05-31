d61495de8a71b7f4446af81541774cc510d4ac91
LMSimpleCoder <1689453839lm@gmail.com>
2022-05-31 14:08:58 +0800

    1

--- src/main/java/com/practice/map/MapTest.java
+++ src/main/java/com/practice/map/MapTest.java
@@ -10,6 +10,8 @@ public class MapTest {
 
     public static void main(String[] args) {
         Map<String, String> map = new HashMap<>(4);
+        map.put(null, "111111");
+        System.out.println(map.get(null));
     }
 
 }
--- src/main/java/com/practice/test/Test.java
+++ src/main/java/com/practice/test/Test.java
@@ -1,6 +1,5 @@
 package com.practice.test;
 
-import com.sun.org.apache.xpath.internal.operations.String;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,8 +9,8 @@ import java.util.List;
  */
 public class Test {
 
- /*   public static void main(String[] args) {
-        *//*String userName = "admin2400";
+    /*   public static void main(String[] args) {
+     *//*String userName = "admin2400";
         String password = "Qwer@1234";
         long timestamp = System.currentTimeMillis();
         String destr = Base64Util.encodeData(userName + "+" + timestamp + "+" + password);
@@ -21,8 +20,17 @@ public class Test {
 
     }*/
 
-    public static void main(String[] args) {
-        List<Object> objects = new ArrayList<>();
-        objects.add(new String());
-    }
+//    public static void main(String[] args) {
+//        List<Object> objects = new ArrayList<>();
+//        objects.add(new String());
+//    }
+
+//    public static void main(String[] args) {
+//
+//        String protocol = "storage2:ftp.FTP-1:d49fc0aca54248ba921d001b49c59358.png:attributes[_uploadtime:1578303288910,_file_name:d49fc0aca54248ba921d001b49c59358.png,_path:danganlc/danganlc/20210902/072600e690a04c25a589db7dc3606e54/]";
+//
+//        protocol = protocol.substring(0, protocol.indexOf(":attributes"));
+//        protocol = protocol.substring(protocol.lastIndexOf(".") + 1);
+//        System.out.println(protocol);
+//    }
 }
